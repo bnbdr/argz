71f64b78bbd13d2d43e5fb960c65cbd9bf4daef5
bnbdr <>
2018-07-14 21:23:01 +0300

    adding setup, link to readme

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+import argz
+
+
+author = 'bnbdr'
+setup(
+    name='argz',
+    version='.'.join(map(str, argz.version)),
+    author=author,
+    author_email='bad.32@outlook.com',
+    url='https://github.com/{}/argz'.format(author),
+    description="Argument parsing for the lazy",
+    long_description=argz.__doc__,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    keywords='argument parse args',
+    py_modules=['argz'],
+    classifiers=(
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
